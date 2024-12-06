0a2cb33ea7b734ad79014d08fec14aee5ff8d2f8
pedro amaral <amaralph1995@gmail.com>
2024-12-06 15:39:51 -0300

    init commit

---
+++ src/main/java/com/amaralph/ds_academic_event/DsAcademicEventApplication.java
@@ -0,0 +1,13 @@
+package com.amaralph.ds_academic_event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAcademicEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAcademicEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amaralph/ds_academic_event/DsAcademicEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amaralph.ds_academic_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAcademicEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
